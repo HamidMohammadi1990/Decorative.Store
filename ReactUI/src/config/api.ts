484bf98fe80a88becf340bab0fc38ba6d8a9ee2f
Hamid Mohammadnian <hamidmohammadnian@gmail.com>
2026-08-02 12:19:41 +0330

    change url

--- ReactUI/src/config/api.ts
+++ ReactUI/src/config/api.ts
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-  (import.meta.env.VITE_API_BASE_URL as string | undefined) ?? 'http://localhost:60928'
+  (import.meta.env.VITE_API_BASE_URL as string | undefined) ?? 'https://localhost:60927'
