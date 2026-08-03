94503e420a4da65dcd54acab84d191e74d30ebbb
HAMID-PC <40312758@PC-292>
2026-08-03 15:12:39 +0330

    buildable porject

--- BackEndProject/src/Core/Store.Application/Common/Security/AuthClaimResolver.cs
+++ BackEndProject/src/Core/Store.Application/Common/Security/AuthClaimResolver.cs
@@ -1,6 +1,7 @@
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using Edition.Application.Models.Constants;
+using Microsoft.AspNetCore.Identity;
 
 namespace Edition.Application.Common.Security;
 
--- BackEndProject/src/Core/Store.Domain/Dtos/Pagination/PagedRequest.cs
+++ BackEndProject/src/Core/Store.Domain/Dtos/Pagination/PagedRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace Store.Domain.Dtos.Pagination;
 
-public abstract record PagedRequest
+public record PagedRequest
 {
     private const int MaxPageSize = 100;
     
